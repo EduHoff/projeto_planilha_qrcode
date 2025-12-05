bcf2d481b499f368d3b2b16320c846f7c303ca8c
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-12-05 12:54:21 -0300

    componente para mostrar status da conexão da planilha entre o Frontend e Backend

---
+++ frontend/src/components/DisplayStatusConnection.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import api from "../api/client";
+
+const DisplayStatusConnection: React.FC = () => {
+  const [message, setMessage] = useState<string>("Carregando...");
+  const [apiError, setApiError] = useState<boolean>(false);
+
+  const userPath = localStorage.getItem("UserPath");
+
+    function isStringEmptyOrNull(txt:string) {
+        return !txt || txt.length === 0;
+    }
+
+  useEffect(() => {
+    api.get("/api/hello")
+      .then(response => {
+
+        const msg = response.data.message;
+        setMessage(msg);
+
+        if (isStringEmptyOrNull(msg)) {
+          setApiError(true);
+        } else {
+          setApiError(false);
+        }
+      })
+      .catch(error => {
+        console.error("Erro ao buscar dados:", error);
+        setMessage("Erro ao conectar com o servidor");
+        setApiError(true);
+      });
+  }, []);
+
+  return (
+    <div>
+
+
+      {/* LOCALSTORAGE */}
+      {userPath ? (
+        <p><strong style={{color:'lightblue'}}>Frontend: </strong> {userPath}</p>
+      ) : (
+        <p style={{ color: "red" }}><strong style={{color:'lightblue'}}>Frontend: </strong>Nenhum caminho encontrado no LocalStorage!</p>
+      )}
+
+      {/* API */}
+      {!apiError ? (
+        <p><strong style={{color:'yellow'}}>Backend: </strong> {message}</p>
+      ) : (
+        <p style={{ color: "red" }}><strong style={{color:'yellow'}}>Backend: </strong>Falha ao conectar com o servidor Python!</p>
+      )}
+
+      {/* Mobile */}
+      <p><strong>Mobile: </strong>Ainda não existe!</p>
+
+      
+    </div>
+  );
+};
+
+export default DisplayStatusConnection;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import api from "../api/client"; 
-import UserPath from "../components/UserPath"; 
+//import React, { useEffect, useState } from 'react';
+import DisplayStatusConnection from '../components/DisplayStatusConnection';
+import UserPath from '../components/UserPath';
 
 
 const Home: React.FC = () => {
-  const [message, setMessage] = useState<string>("Carregando...");
 
-  useEffect(() => {
-    api.get("/api/hello")
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error("Erro ao buscar dados:", error);
-        setMessage("Erro ao conectar com o servidor");
-      });
-  }, []);
 
   return (
     <div>
       <h1>Página Inicial</h1>
       <h2>Aqui é feito a conexão com a planilha local</h2>
-      <p><strong style={{color:'lightblue'}}>React: </strong>{localStorage.getItem("UserPath")}</p>
-      <p><strong style={{color:'yellow'}}>Python: </strong>{message}</p>
-      <UserPath />
+      <DisplayStatusConnection/>
+      <UserPath/>
     </div>
   );
 };
